1266ae0332cbc68c709f3d4e319cad6fafe723af
unknown <tonicveljko@gmail.com>
2020-05-12 15:22:56 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,8 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading ...'
     messageTwo.textContent = ''
     
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    // fetch makes a request to heroku if it exists, only if it doesn't it requests it from localhost
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
